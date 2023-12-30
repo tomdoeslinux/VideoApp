29cb13174d1ceaa80d2acde4d33206207d31741c
tom <tomdoeslinux@protonmail.com>
2023-12-30 14:42:29 +1300

    Add required functionality.

--- App.tsx
+++ App.tsx
@@ -1,18 +1,25 @@
-import React, {useState} from 'react';
+import React, {useRef, useState} from 'react';
 import {Button, Text, useWindowDimensions, View} from 'react-native';
 import Video from 'react-native-video'
 
 function App() {
     const { width, height } = useWindowDimensions()
 
+    const player = useRef<Video>(null)
     const isPortrait = height > width
 
-    const videos = [require('./videos/manhattan.mp4'), require('./videos/flower-valley.mp4')]
+    const videos = [require('./videos/flower-valley.mp4'), require('./videos/manhattan.mp4'), require('./videos/tiger.mp4')]
 
-    const [currentVideo, setCurrentVideo] = useState(videos[1])
+    const [currentVideo, setCurrentVideo] = useState(videos[0])
+    const [paused, setPaused] = useState(true)
 
     if (isPortrait) {
         return <Text style={{ color: 'red' }}>Please change orientation to landscape</Text>
+    }
+
+    function changeVideo(newVideo: any) {
+        setCurrentVideo(newVideo)
+        setPaused(false)
     }
 
     return (
@@ -21,20 +28,37 @@ function App() {
             width: '100%',
             height: '100%'
         }}>
-            <Video style={{ width: '100%', height: '100%' }} resizeMode='cover' source={currentVideo} playInBackground={true} />
+            <Video
+                style={{ width: '100%', height: '100%' }}
+                resizeMode='cover'
+                source={currentVideo}
+                ref={player}
+                paused={paused}
+                onLoad={() => {
+                    if (paused) {
+                        player.current?.seek(0)
+                    }
+                }}
+                onEnd={() => {
+                    setPaused(true)
+                    player.current?.seek(0)
+                }}
+            />
 
-            <View style={{
-                gap: 10,
-                flexDirection: 'row',
-                justifyContent: 'space-evenly',
-                marginTop: 'auto',
-                bottom: 0,
-                marginBottom: 32
-            }}>
-                <Button title="EST"  />
-                <Button title="RUS" />
-                <Button title="ENG" />
-            </View>
+            {paused && (
+                <View style={{
+                    gap: 10,
+                    flexDirection: 'row',
+                    justifyContent: 'space-evenly',
+                    marginTop: 'auto',
+                    bottom: 0,
+                    marginBottom: 32
+                }}>
+                    <Button title="EST" onPress={() => changeVideo(videos[0])} />
+                    <Button title="RUS" onPress={() => changeVideo(videos[1])}  />
+                    <Button title="ENG" onPress={() => changeVideo(videos[2])}  />
+                </View>
+            )}
         </View>
     )
 }
